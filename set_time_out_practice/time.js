17a92589917d1d698a7395f2f5d8ce062d4bce57
Himanshu Dinkar <himanshudinkar43@gmail.com>
2024-10-04 22:07:38 +0530

    practice question

---
+++ set_time_out_practice/time.js
@@ -0,0 +1,8 @@
+function card(){
+    setTimeout(show, 5000)
+
+    function show(){
+        let selecttimeout = document.querySelector('#timeout');
+        selecttimeout.style.display = "block";
+    }
+}
