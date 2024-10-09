db0f8b612dbce9deb30d93d48db9046190b95a00
Himanshu Dinkar <himanshudinkar43@gmail.com>
2024-10-09 21:38:01 +0530

    template literals

---
+++ template literals/index.js
@@ -0,0 +1,14 @@
+// Write a JavaScript function that takes two variables, name and age, 
+//and returns a sentence using template literals:
+
+// "My name is [name] and I am [age] years old."
+
+function templit(){
+    let name = document.querySelector('#name').value;
+    let age = document.querySelector('#age').value;
+
+    let output = document.querySelector('#output');
+    output.innerHTML = `My name is "${name}" and I am "${age}" years old.`;
+
+    return false;
+}
