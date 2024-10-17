07797f04c571977955be55f447121b44e03b01e0
Himanshu Dinkar <himanshudinkar43@gmail.com>
2024-10-17 22:41:37 +0530

    remark

--- array/practice.js
+++ array/practice.js
@@ -1,37 +1,63 @@
-let arr = [46,32,87.6,32,937,947];
-console.log(arr)
+// let arr = [46,32,87.6,32,937,947];
+// console.log(arr)
 
-console.log(arr[3])
+// console.log(arr[3])
 
-let arr1 = [3.35,"hello",455,"himanshu"];
-console.log(arr1[1])
+// let arr1 = [3.35,"hello",455,"himanshu"];
+// console.log(arr1[1])
 
-console.log(arr1[1][4])
+// console.log(arr1[1][4])
 
-let arr3 = [353,"hrllo",445,"himanshu",function(){console.log("function call")}]
+// let arr3 = [353,"hrllo",445,"himanshu",function(){console.log("function call")}]
 
-arr3[4]();
+// arr3[4]();
 
-// function data()
+// // function data()
+// // {
+// //     console.log("himanshu")
+// // }
+// // let arr4 = [344,"hello",data(),67];
+
+// // console.log(arr4(2))
+
+// // let arr5 = [arr,arr1,arr3,arr4];
+
+
+// //method
+
+// let arr6 = [34,"hello",54,98,78];
+
+// arr6.unshift("himanshu");
+// console.log(arr6) 
+
+// let arr7 = [34,54,89,4,2,44,98]
+
+// arr7.splice(1,0,"hello","hi")
+
+// console.log(arr7) 
+
+
+// let number = 1;
+
+// do
 // {
-//     console.log("himanshu")
+//     console.log(number);
+//     number++;
+// }while(number <= 10);
+
+// let arr = [32,43,87,1,54,98,133,1,34];
+
+// let i = 0;
+// while(i < arr.length)
+// {
+//     console.log(arr[i]);
+//     i++;
 // }
-// let arr4 = [344,"hello",data(),67];
 
-// console.log(arr4(2))
+let arr = [76,822,56,928,9,12,];
 
-// let arr5 = [arr,arr1,arr3,arr4];
+for(let i of arr)
+{
+    console.log(i);
+}
 
-
-//method
-
-let arr6 = [34,"hello",54,98,78];
-
-arr6.unshift("himanshu");
-console.log(arr6) 
-
-let arr7 = [34,54,89,4,2,44,98]
-
-arr7.splice(1,0,"hello","hi")
-
-console.log(arr7) 
